0956230292065cc352f9d094a4bfd0c385996c1b
Dlacreme <dlacreme@gmail.com>
2017-12-11 20:29:11 +0700

    v0

---
+++ config/env/env.go
@@ -0,0 +1,61 @@
+// Package env reads the application settings.
+package env
+
+import (
+	"encoding/json"
+
+	"github.com/Dlacreme/gocore/back/email"
+	"github.com/Dlacreme/gocore/back/server"
+	"github.com/Dlacreme/gocore/back/session"
+	"github.com/Dlacreme/gocore/back/storage/driver/mysql"
+	"github.com/Dlacreme/gocore/config/jsonconfig"
+	"github.com/Dlacreme/gocore/view"
+	"github.com/Dlacreme/gocore/view/asset"
+	"github.com/Dlacreme/gocore/webtools/form"
+)
+
+// *****************************************************************************
+// Application Settings
+// *****************************************************************************
+
+// Info structures the application settings.
+type Info struct {
+	Asset    asset.Info    `json:"Asset"`
+	Email    email.Info    `json:"Email"`
+	Form     form.Info     `json:"Form"`
+	MySQL    mysql.Info    `json:"MySQL"`
+	Server   server.Info   `json:"Server"`
+	Session  session.Info  `json:"Session"`
+	Template view.Template `json:"Template"`
+	View     view.Info     `json:"View"`
+	path     string
+}
+
+// Path returns the env.json path
+func (c *Info) Path() string {
+	return c.path
+}
+
+// ParseJSON unmarshals bytes to structs
+func (c *Info) ParseJSON(b []byte) error {
+	return json.Unmarshal(b, &c)
+}
+
+// New returns a instance of the application settings.
+func New(path string) *Info {
+	return &Info{
+		path: path,
+	}
+}
+
+// LoadConfig reads the configuration file.
+func LoadConfig(configFile string) (*Info, error) {
+	// Create a new configuration with the path to the file
+	config := New(configFile)
+
+	// Load the configuration file
+	err := jsonconfig.Load(configFile, config)
+
+	// Return the configuration
+	return config, err
+}
